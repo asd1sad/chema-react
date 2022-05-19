22a109114a63a058ee5cd05cb6926be1acbc418d
ocb <silvestrebosch@hotmail.com>
2022-05-19 17:43:20 -0300

    Navbar - c2 - JSX

--- chema/src/App.js
+++ chema/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+ 
+import './main.css';
+ 
+
+// 1- desestructurando 
+// import {Platos} from './platos.js';
+// 2- default
+// import Platos from './platos.js';
+
+import Navbar from './comoponents/Navbar'
 
 function App() {
+ 
+  const contenido = "Este es el menu de mi restaurant"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+      {/* <Platos/> */}
+      <Navbar/>
+      
+      <h2>{contenido}</h2>
+      
       </header>
     </div>
   );
 }
 
 export default App;
+// class = className
+// {} se escribe js adentro
+// html va en camel case _ fontSize
+// elementos tienen que estar cerrados _ img />
---
+++ chema/src/comoponents/Navbar.js
@@ -0,0 +1,20 @@
+
+
+
+const Navbar = () => {
+
+    return <header>
+
+        <h1> Resto</h1>
+
+        <nav>
+            <ul>
+            <li>CABA</li>
+            <li>Buenos Aires</li>
+            <li>Interior</li>
+            </ul>
+        </nav>
+    </header>
+}
+
+export default Navbar
